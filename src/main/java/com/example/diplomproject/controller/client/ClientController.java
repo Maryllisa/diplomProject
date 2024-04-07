9cd523680a6976156dba4ce8b17eceb0352c9c39
Ania <anna.anna.linnik@gmail.com>
2024-04-08 01:48:49 +0300

    Дурачок делает cmr

--- src/main/java/com/example/diplomproject/controller/client/ClientController.java
+++ src/main/java/com/example/diplomproject/controller/client/ClientController.java
@@ -22,4 +22,6 @@ public class ClientController {
     public String getZavStatus() {return "/client/zavStatus";}
     @GetMapping("/client/prodStatus")
     public String getProdStatus() {return "/client/prodStatus";}
+    @GetMapping("/client/addCMR")
+    public String getAddCMR() {return "/client/addCMR";}
 }
