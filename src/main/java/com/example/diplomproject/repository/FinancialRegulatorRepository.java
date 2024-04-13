d85a5f9c999eef9279b88d65c8963b29b6d856af
Mary <parmo1977@gmail.com>
2024-04-13 15:21:09 +0300

    delete type

--- src/main/java/com/example/diplomproject/repository/FinancialRegulatorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.diplomproject.repository;
-
-import com.example.diplomproject.model.entity.declaration.FinancialRegulator;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FinancialRegulatorRepository extends JpaRepository<FinancialRegulator,Long> {
-}
