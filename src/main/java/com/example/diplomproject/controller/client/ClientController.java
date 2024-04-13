1573d7b07075c87f2cf480eb8b54048e28e712a9
Mary <parmo1977@gmail.com>
2024-04-13 18:58:22 +0300

    add Object for CRM

--- src/main/java/com/example/diplomproject/controller/client/ClientController.java
+++ src/main/java/com/example/diplomproject/controller/client/ClientController.java
@@ -2,6 +2,7 @@ package com.example.diplomproject.controller.client;
 
 import com.example.diplomproject.model.dto.DeclarationDTO;
 import com.example.diplomproject.model.dto.ProductDTO;
+import com.example.diplomproject.model.entity.CRM;
 import com.example.diplomproject.model.entity.declaration.DeclarationTD;
 import com.example.diplomproject.service.DeclarationTDService;
 import lombok.RequiredArgsConstructor;
@@ -19,37 +20,80 @@ public class ClientController {
     private final DeclarationTDService declarationTDService;
 
     @GetMapping("/client")
-    public String getClient() {return "/client/clientPanel";}
+    public String getClient() {
+        return "/client/clientPanel";
+    }
+
     @GetMapping("/client/supplierReg")
-    public String getSupplierReg() {return "/client/supplierReg";}
+    public String getSupplierReg() {
+        return "/client/supplierReg";
+    }
+
     @GetMapping("/client/regOfDeclaration")
     public String getRegOfDeclaration(Model model, Authentication authentication) {
         String login = authentication.getName();
         model.addAttribute("declaration", declarationTDService.geFormForNewDeclaration(login));
         return "/client/regOfDeclaration";
     }
+
     @GetMapping("/client/addStorageRequest")
-    public String getAddStorageRequest() {return "/client/addStorageRequest";}
+    public String getAddStorageRequest() {
+        return "/client/addStorageRequest";
+    }
+
     @GetMapping("/client/addTTN")
-    public String getAddTTN() {return "/client/addTTN";}
+    public String getAddTTN() {
+        return "/client/addTTN";
+    }
+
     @GetMapping("/client/zavStatus")
-    public String getZavStatus() {return "/client/zavStatus";}
+    public String getZavStatus() {
+        return "/client/zavStatus";
+    }
+
     @GetMapping("/client/prodStatus")
-    public String getProdStatus() {return "/client/prodStatus";}
+    public String getProdStatus() {
+        return "/client/prodStatus";
+    }
+
     @GetMapping("/client/addCMR")
-    public String getAddCMR() {return "/client/addCMR";}
+    public String getAddCMR(Model model) {
+        model.addAttribute("crm", new CRM());
+        return "/client/addCMR";
+    }
+
     @GetMapping("/client/regAsAComp")
-    public String getRegAsAComp() {return "/client/regAsAComp";}
+    public String getRegAsAComp() {
+        return "/client/regAsAComp";
+    }
+
     @GetMapping("/client/regAuto")
-    public String getRegAuto() {return "/client/regAuto";}
+    public String getRegAuto() {
+        return "/client/regAuto";
+    }
+
     @GetMapping("/client/showMarkedProduct")
-    public String getShowMarkedProduct() {return "/client/showMarkedProduct";}
+    public String getShowMarkedProduct() {
+        return "/client/showMarkedProduct";
+    }
+
     @GetMapping("/client/addZavForMark")
-    public String getAddZavForMark() {return "/client/addZavForMark";}
+    public String getAddZavForMark() {
+        return "/client/addZavForMark";
+    }
+
     @GetMapping("/client/makeZavForOtp")
-    public String getMakeZavForOtp() {return "/client/makeZavForOtp";}
+    public String getMakeZavForOtp() {
+        return "/client/makeZavForOtp";
+    }
+
     @GetMapping("/client/showBillForSave")
-    public String getShowBillForSave() {return "/client/showBillForSave";}
+    public String getShowBillForSave() {
+        return "/client/showBillForSave";
+    }
+
     @GetMapping("/client/priceForSave")
-    public String getPriceForSave() {return "/client/priceForSave";}
+    public String getPriceForSave() {
+        return "/client/priceForSave";
+    }
 }
