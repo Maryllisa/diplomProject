24fba1224f3560564627a74d5836a9975b154d21
Mary <parmo1977@gmail.com>
2024-04-13 23:48:45 +0300

    add model for GRD

---
+++ src/main/java/com/example/diplomproject/model/entity/GoodTransportDocument.java
@@ -0,0 +1,22 @@
+package com.example.diplomproject.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class GoodTransportDocument {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idGoodTransportDocument;
+    @Column
+    private String goodsTransportDocumentNumbers;
+    @Column
+    @Lob
+    private String srcTransportDocument;
+}
