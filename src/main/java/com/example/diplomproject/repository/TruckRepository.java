14dcfff535491785348e9e646226ed31ee30625d
Mary <parmo1977@gmail.com>
2024-04-14 20:13:16 +0300

    add repository for Truck and Driver

---
+++ src/main/java/com/example/diplomproject/repository/TruckRepository.java
@@ -0,0 +1,9 @@
+package com.example.diplomproject.repository;
+
+import com.example.diplomproject.model.entity.Truck;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TruckRepository extends JpaRepository<Truck, Long> {
+}
